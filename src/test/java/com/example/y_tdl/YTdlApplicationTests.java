9139bf03a1a6e61f15b2c036c3060e546620e69d
YyoungsY <diddud1236@daum.net>
2025-04-06 23:04:06 +0900

    first commit

---
+++ src/test/java/com/example/y_tdl/YTdlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.y_tdl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YTdlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
